e70a90301714f713fc61064bfe650dba032158f1
CristianJPerezMorin <101406605+CristianJPerezMorin@users.noreply.github.com>
2024-04-30 15:24:40 +0100

    Salto realizado

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody _rb;
     private PlayerInput _playerActions;
 
-    public float horizontal, vertical, speed, turnSpeed;
+    public float horizontal, vertical, speed, turnSpeed, jumpForce;
+    private bool _isGrounded, _doJump;
 
     public static PlayerMovement Instance { get; private set; }
 
@@ -33,19 +34,45 @@ public class PlayerMovement : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
 
         speed = 5;
+        jumpForce = 2;
         turnSpeed = 150;
+
+        _isGrounded = false;
+        _doJump = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 move = _playerActions.actions["Move"].ReadValue<Vector2>();
-        //move *= speed;
         Vector3 fwd = (transform.forward * move.y) * speed;
         fwd.y = _rb.velocity.y;
 
 
         _rb.velocity = fwd;
         transform.Rotate(0, move.x * turnSpeed * Time.deltaTime, 0);
+
+        if (_isGrounded)
+        {
+            _doJump = _playerActions.actions["Jump"].ReadValue<float>() == 1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_doJump)
+        {
+            _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            _isGrounded = true;
+            _doJump = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.CompareTag("Terrain")) 
+        {
+            _isGrounded = true;
+        }
     }
 }
